50dca9f28ec570b05754af5440a540b86ec9c720
DESKTOP-7H59EI6\Rasi <csd3312@csd.uoc.gr>
2018-07-17 11:24:32 +0300

    Empty project created with npm + empty componnent new pad just or testign

---
+++ online-editor/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+   <newPad></newPad>
+  </div>
+</template>
+
+<script>
+import newPad from './components/newPad/newPad.vue'
+
+export default {
+  components: {
+    'newPad':newPad
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ online-editor/src/components/newPad/newPad.vue
@@ -0,0 +1,8 @@
+<template  src="./newPad.html" ></template>
+<style  src="./newPad.css" scoped></style>
+
+
+<script>
+export default {
+}
+</script>

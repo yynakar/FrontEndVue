d4aee05d1355362b26c544570154c27ca52631d5
DESKTOP-7H59EI6\Rasi <csd3312@csd.uoc.gr>
2018-07-17 14:02:44 +0300

    event Recognition with console logs

--- online-editor/src/components/newPad/newPad.vue
+++ online-editor/src/components/newPad/newPad.vue
@@ -4,5 +4,30 @@
 
 <script>
 export default {
+    name:"newPad",
+    methods:{
+        selectEvent: function(event){
+            console.log("Select event!");
+            console.log(event);
+        },
+        keyDownEvent: function(){
+            console.log("keyDownEvent!");
+        },
+        clickEvent: function(event){
+            console.log("clickEvent!");
+            console.log(event);
+        },
+        pasteEvent: function(){
+            console.log("pasteEvent");
+        },
+        cutEvent: function(){
+            console.log("cutEvent");
+        },
+
+
+    },
+    mounted(){
+        console.log(this.$refs);
+    }
 }
 </script>

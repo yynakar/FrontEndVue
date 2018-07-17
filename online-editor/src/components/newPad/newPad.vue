0fd0f4cd9593781bafdaf3c9752753ff0fe40952
DESKTOP-7H59EI6\Rasi <csd3312@csd.uoc.gr>
2018-07-17 17:02:17 +0300

    almost finished textarea events recognitions :cut paste select keydown etc returning the right info

--- online-editor/src/components/newPad/newPad.vue
+++ online-editor/src/components/newPad/newPad.vue
@@ -7,21 +7,60 @@ export default {
     name:"newPad",
     methods:{
         selectEvent: function(event){
-            console.log("Select event!");
-            console.log(event);
+
+            var length = event.target.selectionEnd-event.target.selectionStart;
+
+            var info = {
+                type:'selectEvent',
+                selectionEnd:event.target.selectionEnd,
+                selectionStart:event.target.selectionStart,
+                length:length
+            }
+
+            console.log(info);
+            return info;
         },
-        keyDownEvent: function(){
-            console.log("keyDownEvent!");
+        keyDownEvent: function(event){
+            //BE WARE! textcursor starts from 0
+
+           var info = {
+                type:'keyDownEvent',
+                key:event.key,
+                textCursor:event.target.selectionEnd
+            }
+
+            // console.log(event);
+            console.log(info);
+            return info;
         },
         clickEvent: function(event){
-            console.log("clickEvent!");
-            console.log(event);
+            //console.log("clickEvent!");
+            //console.log(event);
         },
-        pasteEvent: function(){
-            console.log("pasteEvent");
+        pasteEvent: function(event){
+            //our text cursor has the value of the text cursor position
+            //the user had excactly before the paste event
+
+            var info = {
+                type:'pasteEvent',
+                string:event.clipboardData.getData('Text'),
+                length:event.clipboardData.getData('Text').length,
+                textCursor: event.target.selectionEnd
+            }
+
+            console.log(info);
+            return info;
         },
-        cutEvent: function(){
+        cutEvent: function(event){
+            var info = {
+                type:'cutEvent',
+                string:event.clipboardData.getData('Text'),
+                length:event.clipboardData.getData('Text').length,
+                textCursor: event.target.selectionEnd
+            }
             console.log("cutEvent");
+            console.log(event);
+            console.log(info);
         },
 
 

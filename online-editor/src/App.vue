6524a8f6807c01e12c3a623da0da1c5cca5bb486
aimilios <aimiliosg4@gmail.com>
2018-07-20 12:26:43 +0300

    navbar me ekremothta to img

--- online-editor/src/App.vue
+++ online-editor/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-   <newPad></newPad>
+    
+    <navbar> </navbar>
+    <maincontent></maincontent>
+    <newPad></newPad>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import navbar from './components/navbar/navbar.vue'
 import newPad from './components/newPad/newPad.vue'
+import MainContent from './components/MainContent/MainContent.vue'
 
 export default {
   components: {
-    'newPad':newPad
+    'newPad':newPad,
+    'navbar':navbar,
+    'maincontent':MainContent
   }
 }
 </script>
---
+++ online-editor/src/components/navbar/navbar.vue
@@ -0,0 +1,12 @@
+<template  src="./navbar.html" ></template>
+<style  src="./navbar.css" scoped></style>
+
+
+<script>
+export default {
+    name:"navbar",
+    methods:{   
+        
+    }
+}
+</script>
